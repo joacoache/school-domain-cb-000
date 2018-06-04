a09bb5b4560f24ee2250c4ec2328dd8f6f220726
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-06-04 22:14:47 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+class School
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  roster = {}
+
+  def add_student(name, grade)
+    if roster.key?(grade) do
+      roster[grade] << name
+    else
+      roster[grade] = []
+      roster[grade] << name
+    end
+    roster
+  end
+
+  def grade(grade)
+    roster[grade]
+  end 
+
+  def sort
+    roster.each do |a, b|
+      b.each do |c|
+        c.sort
+      end
+    end
+  end    
+
+
+end
